dabce7f07695f5541620ad3d619c33a217754c1f
Ben Cooper <cooperben83@gmail.com>
2024-10-07 20:28:45 +0100

    changed header opacity

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -3,7 +3,9 @@
 	const { projects, currentProject } = data;
 </script>
 
-<header class="w-full bg-bg-2 px-10 py-2 shadow-lg">
+<header
+	class="absolute top-0 z-10 w-full bg-bg-2 px-10 py-2 opacity-25 shadow-lg transition-opacity duration-200 hover:opacity-80"
+>
 	<div class="flex h-full w-1/3 flex-row gap-10">
 		{currentProject.name}
 	</div>
