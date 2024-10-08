4df374bcecdd8ff97db4f1dddd956a653f5ae606
Ben Cooper <cooperben83@gmail.com>
2024-10-08 11:30:07 +0100

    updated environment vars

--- README.md
+++ README.md
@@ -35,4 +35,6 @@ npm run build
 
 You can preview the production build with `npm run preview`.
 
-> To deploy your app, you may need to install an [adapter](https://kit.svelte.dev/docs/adapters) for your target environment.
+## Deploying
+
+I'm doing this through Vercel
--- src/lib/utils/pb.ts
+++ src/lib/utils/pb.ts
@@ -1,8 +1,9 @@
 import { SECRET_PB_EMAIL, SECRET_PB_PASSWORD } from '$env/static/private';
+import { PUBLIC_PB_URL } from '$env/static/public';
 import PocketBase from 'pocketbase';
 
 export async function getPBClient() {
-	const pb = new PocketBase('http://127.0.0.1:8090');
+	const pb = new PocketBase(PUBLIC_PB_URL);
 	await pb.admins.authWithPassword(SECRET_PB_EMAIL, SECRET_PB_PASSWORD);
 
 	return pb;
